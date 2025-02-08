c23d99def6923b9c7f9deb162126c474fa77532b
Mohamad Noor Alim Hussin <mnalim@efinixinc.com>
2025-02-09 01:20:23 +0800

    package/startup-notification: add version selection
    Some packages require to use newer version of startup-notification
    library which currenly does not support by the package makefile.
    This patch created a version selection for this package.
    
    Signed-off-by: Mohamad Noor Alim Hussin <mnalim@efinixinc.com>

--- package/startup-notification/startup-notification.mk
+++ package/startup-notification/startup-notification.mk
@@ -3,11 +3,15 @@
 # startup-notification
 #
 ################################################################################
-
-STARTUP_NOTIFICATION_VERSION = 0.9
+STARTUP_NOTIFICATION_VERSION = $(call qstrip,$(BR2_STARTUP_NOTIFICATION_VERSION))
 STARTUP_NOTIFICATION_SITE = http://freedesktop.org/software/startup-notification/releases
 STARTUP_NOTIFICATION_INSTALL_STAGING = YES
 STARTUP_NOTIFICATION_DEPENDENCIES = xlib_libX11
+
+ifeq ($(BR2_STARTUP_NOTIFICATION_VERSION_0_12), y)
+STARTUP_NOTIFICATION_DEPENDENCIES += xcb-util
+endif
+
 STARTUP_NOTIFICATION_CONF_ENV = lf_cv_sane_realloc=yes
 STARTUP_NOTIFICATION_CONF_OPTS = --with-x \
 	--x-includes="$(STAGING_DIR)/usr/include/X11" \
