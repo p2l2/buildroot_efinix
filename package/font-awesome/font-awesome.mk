eb1a8cb9364b9ca2a504383e2297714db6d25168
Mohamad Noor Alim Hussin <mnalim@efinixinc.com>
2025-02-18 00:09:10 +0800

    package/font-awesome: copy the fonts to standard fonts directory
    Signed-off-by: Mohamad Noor Alim Hussin <mnalim@efinixinc.com>

--- package/font-awesome/font-awesome.mk
+++ package/font-awesome/font-awesome.mk
@@ -10,9 +10,9 @@ FONT_AWESOME_LICENSE = OFL-1.1 (font), MIT (CSS, LESS and Sass files)
 FONT_AWESOME_DIRECTORIES_LIST = css fonts less scss
 
 define FONT_AWESOME_INSTALL_TARGET_CMDS
-	mkdir -p $(TARGET_DIR)/usr/share/font-awesome/
+	mkdir -p $(TARGET_DIR)/usr/share/fonts/font-awesome/
 	$(foreach d,$(FONT_AWESOME_DIRECTORIES_LIST),\
-		cp -dpfr $(@D)/$(d) $(TARGET_DIR)/usr/share/font-awesome$(sep))
+		cp -dpfr $(@D)/$(d) $(TARGET_DIR)/usr/share/fonts/font-awesome$(sep))
 endef
 
 $(eval $(generic-package))
