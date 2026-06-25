ddeb85b48b01c63207f84356f4008518b71f967b
Bernhard Wandl <wandl@p2l2.com>
2026-06-25 13:13:37 +0200

    add go compiler support for RV64

--- package/go/go.mk
+++ package/go/go.mk
@@ -65,6 +65,8 @@ else ifeq ($(BR2_mips64el),y)
 GO_GOARCH = mips64le
 else ifeq ($(BR2_s390x),y)
 GO_GOARCH = s390x
+else ifeq ($(BR2_riscv)$(BR2_ARCH_IS_64),yy)
+GO_GOARCH = riscv64
 endif
 
 # For the convienience of target packages.
